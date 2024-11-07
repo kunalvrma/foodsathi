aa4160dca373a2e4d7f0018d5d12b0aa86193c40
kermaofficial <kunalverma3369@gmail.com>
2024-11-07 13:45:19 +0530

    some minor changes
    01. package.json updated start script to show no warnings,
    02. home.js updated it so there are no warnings

--- my-frontend/src/Components/Home/Home.js
+++ my-frontend/src/Components/Home/Home.js
@@ -1,14 +1,8 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+
 import './home.css';
 
 const Home = () => {
-  const navigate = useNavigate();
-
-  const handleLoginClick = () => {
-    navigate('/login'); // This is correct
-  };
-
+  
   
 };
 
