19f20e4267b12d2ea20d3dd4f082633feb4c5919
kermaofficial <kunalverma3369@gmail.com>
2024-11-08 02:40:16 +0530

    connected frontend to backend
    01. register.js to user.js
    02. my-frontend/services

--- my-backend/models/User.js
+++ my-backend/models/User.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
     role: { type: String, enum: ['ngo', 'restaurant'], required: true }, // Role-based access
     phoneNumber: { type: String, required: true },
 });
