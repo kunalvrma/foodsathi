322e9845e6de0fd278fc9597a6cbb2ba2e0cd2a4
Aryan dixit <aryandixit7629@gmail.com>
2024-11-09 13:24:41 +0530

    app.js update

--- my-frontend/src/App.js
+++ my-frontend/src/App.js
@@ -1,66 +1,86 @@
 import React from 'react';
-import './app.css';
-import Dashboard from './Components/Dashboard/RestaurantDashboard/RestaurantDashboard';
-import Profile from './Components/Dashboard/RestaurantDashboard/Profile';
-import FoodSathies from './Components/FoodSathies/FoodSathies';
-import Home from './Components/Home/Home';
-import ImageCardSection from './Components/ImageCardSection/ImageCard';
-import JourneyCount from './Components/JourneyCount/JourneyCount';
-import Login from './Components/Login';
-import About from './Components/support/About';
-import Navbar from './Components/Navbar/Navbar';
-import Learn from './Components/Learn/Learn';
-import Register from './Components/Register';
-import VideoSection from './Components/VideoSection/VideoSection';
-import { UserProvider } from './UserContext';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import ChatBot from './Components/ChatBot/chatBot';
-import Settings from './Components/Dashboard/RestaurantDashboard/Settings/Settings';
-import DonationForm from './Components/DonationForm/DonationForm';
-import DonationRequestForm from './Components/DonationRequestForm/DonationRequestForm';
-import DonorTracking from './Components/DonationTracking/DonorTracking';
-import RecieverTracking from './Components/DonationTracking/RecieverTracking';
-import ContactUs from './Components/support/ContactUs';
+import './app.css';
+import Navbar from './Components/Navbar/Navbar';
 import ProtectedRoute from './Components/ProtectedRoutes';
-import PostDonation from './Components/Dashboard/RestaurantDashboard/PostDonation';
+import { UserProvider } from './UserContext';
+
+// Lazy-loaded components
+const HomePage = React.lazy(() => import('./Components/Home/Home'));
+const Dashboard = React.lazy(() => import('./Components/Dashboard/RestaurantDashboard/RestaurantDashboard'));
+const Settings = React.lazy(() => import('./Components/Dashboard/RestaurantDashboard/Settings/Settings'));
+const Profile = React.lazy(() => import('./Components/Dashboard/RestaurantDashboard/Profile'));
+const PostDonation = React.lazy(() => import('./Components/Dashboard/RestaurantDashboard/PostDonation'));
+const DonationForm = React.lazy(() => import('./Components/DonationForm/DonationForm'));
+const DonationRequestForm = React.lazy(() => import('./Components/DonationRequestForm/DonationRequestForm'));
+const DonorTracking = React.lazy(() => import('./Components/DonationTracking/DonorTracking'));
+const ReceiverTracking = React.lazy(() => import('./Components/DonationTracking/RecieverTracking'));
+const ContactUs = React.lazy(() => import('./Components/support/ContactUs'));
+const Login = React.lazy(() => import('./Components/Login'));
+const Register = React.lazy(() => import('./Components/Register'));
+const Learn = React.lazy(() => import('./Components/Learn/Learn'));
+const About = React.lazy(() => import('./Components/support/About'));
+const Gallery = React.lazy(() => import('./Components/Gallery/Gallery'));
+const FoodSathies = React.lazy(() => import('./Components/FoodSathies/FoodSathies'));
+const VideoSection = React.lazy(() => import('./Components/VideoSection/VideoSection'));
+const ImageCardSection = React.lazy(() => import('./Components/ImageCardSection/ImageCard'));
+const JourneyCount = React.lazy(() => import('./Components/JourneyCount/JourneyCount'));
+const ChatBot = React.lazy(() => import('./Components/ChatBot/chatBot'));
+const NotFound = () => <h1>404 - Page Not Found</h1>; // Simple 404 Page
 
 const App = () => {
   return (
     <UserProvider>
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={
-          <>
-          <Home />
-          <VideoSection />
-          <ImageCardSection />
-          <FoodSathies/>
-          <JourneyCount /> 
-          <ChatBot/>   </>} />
-         
-        <Route path="/login" element={<Login />} /> 
-        <Route path="/register" element={<Register />} />
-        <Route path="/learn" element={<Learn /> } />
-        <Route path="/restaurant-dashboard" element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute> } />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/postDonation" element={<PostDonation />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/donationForm" element={<DonationForm/>} />
-        <Route path="/donationRequestForm" element={<DonationRequestForm/>} />
-        <Route path="/track-donation-donor" element={<DonorTracking />} />
-        <Route path="/track-donation-receiver" element={<RecieverTracking />} />
-        <Route path="/ContactUs" element={<ContactUs />} />
-       {//<Route path="/about" element={<About/>} />
-       } 
+      <Router>
+        <Navbar />
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            {/* Public Routes */}
+            <Route 
+              path="/" 
+              element={
+                <>
+                  <HomePage />
+                  <VideoSection />
+                  <ImageCardSection />
+                  <FoodSathies />
+                  <JourneyCount />
+                  <ChatBot />
+                  <DonationRequestForm/>
+                  
+                </>
+              } 
+            />
+            <Route path="/AboutUs" element={ <About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/learn" element={<Learn />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/contactUs" element={<ContactUs />} />
+            <Route path="/Request Donation" element={<DonationRequestForm/>} />
+            <Route path="/Donation Form" element={<DonationForm/>} />
+            {/* Protected Routes */}
+            <Route 
+              path="/restaurant-dashboard" 
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              } 
+            />
+            <Route path="/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} />
+            <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+            <Route path="/post-donation" element={<ProtectedRoute><PostDonation /></ProtectedRoute>} />
+            <Route path="/donation-form" element={<ProtectedRoute><DonationForm /></ProtectedRoute>} />
+            <Route path="/donation-request-form" element={<ProtectedRoute><DonationRequestForm /></ProtectedRoute>} />
+            <Route path="/track-donation-donor" element={<ProtectedRoute><DonorTracking /></ProtectedRoute>} />
+            <Route path="/track-donation-receiver" element={<ProtectedRoute><ReceiverTracking /></ProtectedRoute>} />
 
-
-      </Routes>
-     
-    </Router>
+            {/* Fallback Route */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </React.Suspense>
+      </Router>
     </UserProvider>
   );
 };
