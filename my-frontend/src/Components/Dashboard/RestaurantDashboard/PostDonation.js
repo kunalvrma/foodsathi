9c3685d60b76f4cca3264413688d3b7b7119f803
Aditi Kushwaha <137718055+AditiKushwaha01@users.noreply.github.com>
2025-05-28 01:43:49 +0530

    Update PostDonation.js

--- my-frontend/src/Components/Dashboard/RestaurantDashboard/PostDonation.js
+++ my-frontend/src/Components/Dashboard/RestaurantDashboard/PostDonation.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { MdOutlineKeyboardDoubleArrowLeft } from "react-icons/md";
 import './PostDonation.css';
 
 const PostDonation = () => {
